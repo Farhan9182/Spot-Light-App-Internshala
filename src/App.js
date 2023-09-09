98f2b3d3e17937c34bbe5e0ac37ee5083cb0e8a1
Farhan Khan <farhaankhaan930@gmail.com>
2023-09-10 01:18:50 +0530

    added eventData context, and eventDetails page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import "./styles.css"
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './components/Routes';
 import { CardDataProvider } from './components/CardDataContext';
+import {EventDataProvider} from './components/EventDataContext'
 
 const App = () => {
   return (
     <Router>
+      <EventDataProvider>
       <CardDataProvider>
         <AppRoutes />
       </CardDataProvider>
+      </EventDataProvider>
     </Router>
   );
 };
