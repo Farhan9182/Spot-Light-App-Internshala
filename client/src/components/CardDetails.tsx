9af53623cc37600955c3551d41b042c8ccb738b6
Farhan Khan <farhaankhaan930@gmail.com>
2023-09-11 17:17:39 +0530

    syncing up changes in client dir

--- client/src/components/CardDetails.tsx
+++ client/src/components/CardDetails.tsx
@@ -1,37 +1,23 @@
 import { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { useEventData } from './EventDataContext';
+import { useCardData } from './CardDataContext';
 
-interface EventDetailsData {
+interface CardDetailsData {
   id: string;
   year: string;
   eventType: string;
-  eventHeader: string;
-  provider: string;
-  facility: string;
   serviceDate: string;
   resourceType: string;
-  cost: string;
 }
 
-function EventDetails() {
+function CardDetails() {
   const location = useLocation();
   const navigate = useNavigate();
   const queryParams = new URLSearchParams(location.search);
-  const index = Number(queryParams.get('id'));
-  const { eventData } = useEventData();
+  const resourceType = queryParams.get('resourceType');
+  const { cardData } = useCardData();
   const [activeTab, setActiveTab] = useState<'summary' | 'data'>('summary');
-  const eventDetailsData: EventDetailsData = eventData[index] || {
-    id: '',
-    year: '',
-    eventType: '',
-    eventHeader: '',
-    provider: '',
-    facility: '',
-    serviceDate: '',
-    resourceType: '',
-    cost: '',
-  };
+  const cardDetailsData: CardDetailsData[] = cardData[resourceType || '']?.cards || []; // Update the property names
 
   // Function to close the details view and go back
   const handleClose = () => {
@@ -58,7 +44,7 @@ function EventDetails() {
               }`}
               onClick={() => setActiveTab('data')}
             >
-              Linked
+              Data
             </button>
             <div className='px-16 border-b-4'></div>
           </div>
@@ -70,54 +56,40 @@ function EventDetails() {
             X
           </button>
         </div>
-        {activeTab === 'data' && (
+        {activeTab === 'summary' && (
           <div className="bg-white p-4 rounded-lg">
             {/* Summary content */}
-            <p className='mt-6 font-bold'>You have {eventData.length} records, current index: {(index || 0) + 1} </p>
+            <p className='mt-6 font-bold'>You have {cardDetailsData.length} records of resource type: {resourceType} </p>
           </div>
         )}
-        {activeTab === 'summary' && (
+        {activeTab === 'data' && (
           <div className="bg-white-200 p-4 rounded-lg mt-4">
             {/* Data content */}
-              <div className='border-b-4 border-gray-200 mt-2 mb-2' key={index || '0'}>
+            {cardDetailsData.map((item, index) => (
+              <div className='border-b-4 border-gray-200 mt-2 mb-2' key={index}>
                 {/* Render data content here based on item */}
                 <div className='flex'>
                   <p className='w-1/2 text-gray-500 text-base'>Id: </p>
-                  <p className='text-lg'>{eventDetailsData?.id}</p>
+                  <p className='text-lg'>{item?.id}</p>
                 </div>
                 <div className='flex'>
                   <p className='w-1/2 text-gray-500 text-base'>Year: </p>
-                  <p className='text-lg'>{eventDetailsData?.year}</p>
+                  <p className='text-lg'>{item?.year}</p>
                 </div>
                 <div className='flex'>
                   <p className='w-1/2 text-gray-500 text-base'>Event Type: </p>
-                  <p className='text-lg'>{eventDetailsData?.eventType}</p>
-                </div>
-                <div className='flex'>
-                  <p className='w-1/2 text-gray-500 text-base'>Event Header: </p>
-                  <p className='text-lg'>{eventDetailsData?.eventHeader}</p>
-                </div>
-                <div className='flex'>
-                  <p className='w-1/2 text-gray-500 text-base'>Provider: </p>
-                  <p className='text-lg'>{eventDetailsData?.provider}</p>
-                </div>
-                <div className='flex'>
-                  <p className='w-1/2 text-gray-500 text-base'>Facility: </p>
-                  <p className='text-lg'>{eventDetailsData?.facility}</p>
+                  <p className='text-lg'>{item?.eventType}</p>
                 </div>
                 <div className='flex'>
                   <p className='w-1/2 text-gray-500 text-base'>Service Date: </p>
-                  <p className='text-lg'>{eventDetailsData?.serviceDate?.split("T")[0]} {eventDetailsData?.serviceDate?.split("T")[1]?.split(":")[0]}:{eventDetailsData?.serviceDate?.split("T")[1]?.split(":")[1]}</p>
+                  <p className='text-lg'>{item?.serviceDate.split("T")[0]} {item?.serviceDate.split("T")[1].split(":")[0]}:{item?.serviceDate.split("T")[1].split(":")[1]}</p>
                 </div>
                 <div className='flex'>
                   <p className='w-1/2 text-gray-500 text-base'>Resource Type: </p>
-                  <p className='text-lg'>{eventDetailsData?.resourceType}</p>
-                </div>
-                <div className='flex'>
-                  <p className='w-1/2 text-gray-500 text-base'>Cost: </p>
-                  <p className='text-lg'>{eventDetailsData?.cost}</p>
+                  <p className='text-lg'>{item?.resourceType}</p>
                 </div>
               </div>
+            ))}
           </div>
         )}
       </main>
@@ -125,4 +97,4 @@ function EventDetails() {
   );
 }
 
-export default EventDetails;
+export default CardDetails;
--- client/src/components/EventDetails.tsx
+++ client/src/components/EventDetails.tsx
@@ -1,24 +1,38 @@
 import { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { useCardData } from './CardDataContext';
+import { useEventData } from './EventDataContext';
 
-interface CardDetailsData {
+interface EventDetailsData {
   id: string;
   year: string;
   eventType: string;
+  eventHeader: string;
+  provider: string;
+  facility: string;
   serviceDate: string;
   resourceType: string;
+  cost: string;
 }
 
-function CardDetails() {
+function EventDetails() {
   const location = useLocation();
   const navigate = useNavigate();
   const queryParams = new URLSearchParams(location.search);
-  const resourceType = queryParams.get('resourceType');
-  const { cardData } = useCardData();
+  const index = queryParams.get('id');
+  const { eventData } = useEventData();
   const [activeTab, setActiveTab] = useState<'summary' | 'data'>('summary');
-  const cardDetailsData: CardDetailsData[] = cardData[resourceType || ''] || [];
-
+  const eventDetailsData: EventDetailsData | undefined = eventData.find(item => item.id == index) || {
+    id: '',
+    year: '',
+    eventType: '',
+    eventHeader: '',
+    provider: '',
+    facility: '',
+    serviceDate: '',
+    resourceType: '',
+    cost: '',
+  };
+  
   // Function to close the details view and go back
   const handleClose = () => {
     navigate(-1); // Go back to the previous page
@@ -44,7 +58,7 @@ function CardDetails() {
               }`}
               onClick={() => setActiveTab('data')}
             >
-              Data
+              Linked
             </button>
             <div className='px-16 border-b-4'></div>
           </div>
@@ -56,40 +70,54 @@ function CardDetails() {
             X
           </button>
         </div>
-        {activeTab === 'summary' && (
+        {activeTab === 'data' && (
           <div className="bg-white p-4 rounded-lg">
             {/* Summary content */}
-            <p className='mt-6 font-bold'>You have {cardDetailsData.length} records of resource type: {resourceType} </p>
+            <p className='mt-6 font-bold'>You have {eventData.length} records, current index: {index} </p>
           </div>
         )}
-        {activeTab === 'data' && (
+        {activeTab === 'summary' && (
           <div className="bg-white-200 p-4 rounded-lg mt-4">
             {/* Data content */}
-            {cardDetailsData.map((item, index) => (
-              <div className='border-b-4 border-gray-200 mt-2 mb-2' key={index}>
+              <div className='border-b-4 border-gray-200 mt-2 mb-2' key={index || '0'}>
                 {/* Render data content here based on item */}
                 <div className='flex'>
                   <p className='w-1/2 text-gray-500 text-base'>Id: </p>
-                  <p className='text-lg'>{item?.id}</p>
+                  <p className='text-lg'>{eventDetailsData?.id}</p>
                 </div>
                 <div className='flex'>
                   <p className='w-1/2 text-gray-500 text-base'>Year: </p>
-                  <p className='text-lg'>{item?.year}</p>
+                  <p className='text-lg'>{eventDetailsData?.year}</p>
                 </div>
                 <div className='flex'>
                   <p className='w-1/2 text-gray-500 text-base'>Event Type: </p>
-                  <p className='text-lg'>{item?.eventType}</p>
+                  <p className='text-lg'>{eventDetailsData?.eventType}</p>
+                </div>
+                <div className='flex'>
+                  <p className='w-1/2 text-gray-500 text-base'>Event Header: </p>
+                  <p className='text-lg'>{eventDetailsData?.eventHeader}</p>
+                </div>
+                <div className='flex'>
+                  <p className='w-1/2 text-gray-500 text-base'>Provider: </p>
+                  <p className='text-lg'>{eventDetailsData?.provider}</p>
+                </div>
+                <div className='flex'>
+                  <p className='w-1/2 text-gray-500 text-base'>Facility: </p>
+                  <p className='text-lg'>{eventDetailsData?.facility}</p>
                 </div>
                 <div className='flex'>
                   <p className='w-1/2 text-gray-500 text-base'>Service Date: </p>
-                  <p className='text-lg'>{item?.serviceDate.split("T")[0]} {item?.serviceDate.split("T")[1].split(":")[0]}:{item?.serviceDate.split("T")[1].split(":")[1]}</p>
+                  <p className='text-lg'>{eventDetailsData?.serviceDate?.split("T")[0]} {eventDetailsData?.serviceDate?.split("T")[1]?.split(":")[0]}:{eventDetailsData?.serviceDate?.split("T")[1]?.split(":")[1]}</p>
                 </div>
                 <div className='flex'>
                   <p className='w-1/2 text-gray-500 text-base'>Resource Type: </p>
-                  <p className='text-lg'>{item?.resourceType}</p>
+                  <p className='text-lg'>{eventDetailsData?.resourceType}</p>
+                </div>
+                <div className='flex'>
+                  <p className='w-1/2 text-gray-500 text-base'>Cost: </p>
+                  <p className='text-lg'>{eventDetailsData?.cost}</p>
                 </div>
               </div>
-            ))}
           </div>
         )}
       </main>
@@ -97,4 +125,4 @@ function CardDetails() {
   );
 }
 
-export default CardDetails;
+export default EventDetails;
